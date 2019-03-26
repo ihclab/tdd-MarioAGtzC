7930146d561f060e5a37485fb3d17a04497203b2
Mario <32181249+MarioAGtzC@users.noreply.github.com>
2019-03-26 17:53:18 -0600

    Burbuja arreglado

--- Ordenamiento/Burbuja.cs
+++ Ordenamiento/Burbuja.cs
@@ -5,11 +5,14 @@ using System.Text;
 namespace Ordenamiento {
     public class Burbuja {
         public void Ordenar(int[] arr) {
-            for (int i = 0; i < arr.Length - 1; i++) {
-                if (arr[i] > arr[i + 1]) {
-                    int aux = arr[i];
-                    arr[i] = arr[i + 1];
-                    arr[i + 1] = aux;
+            for (int i = 1; i < arr.Length; i++) {
+                for (int j = arr.Length - 1; j >= i; j--) {
+                    if (arr[j - 1] > arr[j])
+                    {
+                        int aux = arr[j - 1];
+                        arr[j - 1] = arr[j];
+                        arr[j] = aux;
+                    }
                 }
             }
         }
